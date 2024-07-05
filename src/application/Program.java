0df53f2e08a9571edba7dd6a73d04b7843f1889e
Caio Martins Pinheiro <caiomp10@gmail.com>
2024-07-04 22:20:29 -0300

    Interface created: OnlinePaymentService

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,30 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+import entities.Contract;
+
 public class Program {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Entre com os dados do contrato:");
+        System.out.print("Número: ");
+        int number = sc.nextInt();
+        System.out.print("Data (dd/MM/yyyy): ");
+        LocalDate date = LocalDate.parse(sc.next(), fmt);
+        System.out.print("Valor do contrato: ");
+        double totalValue = sc.nextDouble();
+
+        Contract contract = new Contract(number, date, totalValue);
+
+        System.out.print("Entre com o número de parcelas: ");
+        int n = sc.nextInt();
+        
+        sc.close();
     }
 }
